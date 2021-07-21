8699b8a069e744a0fdab7ab8f9280b26f1e53023
Daniel Salazar <d3_neux@live.com>
2021-07-21 00:05:34 -0500

    Nice express examples

---
+++ 11 Express/03BootstrapDemo/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.get('/', (req, res) => {
+    //res.send('Hi!');
+    res.render('home', {title: "HOME PAGE"});
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    
+    // console.log(data);
+    if (data) {
+        res.render('subreddit', { ...data, title: data["name"] });
+    } else {
+        res.render('notfound', { subreddit });
+    }
+});
+
+app.get('/rand', (req, res) => {
+    const number = Math.floor(Math.random() * 30);
+    res.render('random', { number, title: 'random' });
+});
+
+app.get('/cats', (req, res) => {
+    const cats = ['blue', 'rocket', 'another cat', 'pinky'];
+
+    res.render('cats', { cats , title: 'cats'});
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+    // console.dir(redditData);
+});
+
